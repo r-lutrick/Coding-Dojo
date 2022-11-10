7107c39a2eba7256d02919dfcd545d2aa041f1af
Robert Lutrick <r.lutrick@icloud.com>
2022-11-10 17:49:09 -0600

    File system Creation & Update

--- Web_Fundamentals/Algorithms/W1D4.js
+++ Web_Fundamentals/Algorithms/W1D4.js
@@ -1,4 +1,12 @@
-// ---------------------------------------------------
+/*
+Code Flow - Is the Array a Palindrome
+A palindrome is a word that is spelled the same if read forwards or backwards. 
+Some excellent palindrome examples are:
+racecar
+tacocat
+But in our case we'll be looking at some arrays, can the values in the array be 
+read the same forwards as backwards
+*/
 function isPal(arr) {
     for(var left=0; left<arr.length/2; left++) {
         var right = arr.length-1-left;
