abaa47a56cf5bc30ebe49305ee03f293560e2322
Robert Lutrick <r.lutrick@icloud.com>
2022-11-14 12:10:46 -0600

    Update W2D1.js

--- Web_Fundamentals/Algorithms/W2D1.js
+++ Web_Fundamentals/Algorithms/W2D1.js
@@ -31,7 +31,12 @@ var lifesAnswers = [
 ];
 
 function oracle (arr) {
-    var i = Math.floor(Math.random() * arr.length); // using arr.length allows for different sized arrays. Floor is used here because we want to include the 0.
+    var i = Math.floor(Math.random() * arr.length);
+    // Above can be written like this as well:
+    // var i = Math.random()  < set variable i to be a random number (0.01 - 0.99)
+    // i = i * arr.lenth      < update itself with lenth. Using arr.length allows for different sized arrays. 
+    // i = Math.floor(i)      < update itself with floor. Floor is used here because we want to include the 0.
+    
     return arr[i]
 }
 
