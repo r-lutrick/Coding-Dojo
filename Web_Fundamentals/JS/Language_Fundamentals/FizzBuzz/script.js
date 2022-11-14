8169dc0f512d92527fa86faca6264b99d020dfec
Robert Lutrick <r.lutrick@icloud.com>
2022-11-14 08:49:17 -0600

    Syntax correction

--- Web_Fundamentals/JS/Language_Fundamentals/FizzBuzz/script.js
+++ Web_Fundamentals/JS/Language_Fundamentals/FizzBuzz/script.js
@@ -20,6 +20,6 @@
 for (var i=1; i<=100; i++){
     // var out = ((i%3!=0) && i) || "Fizz" // Version2.1 (fail)
     // var out = ( ((i%3==0) && "Fizz") + ((i%5==0) && "Buzz") ) || i // Version 2.2 (fail)
-    var out = (i%3!=0 && i%5!=0) && i || (i%3==0 && i%5==0) && "FizzBuzz" || (i%3!=0) && "Buzz" || "Fizz" // Version 2.3
+    var out = (i%3!==0 && i%5!==0) && i || (i%3==0 && i%5==0) && "FizzBuzz" || (i%3!==0) && "Buzz" || "Fizz" // Version 2.3
     console.log(out);
 }
