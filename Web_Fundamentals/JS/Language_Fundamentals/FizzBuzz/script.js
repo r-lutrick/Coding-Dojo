db838bc307da166da180be6df88a23d3202185dc
Robert Lutrick <r.lutrick@icloud.com>
2022-11-13 22:05:43 -0600

    FizzBuzz creation

---
+++ Web_Fundamentals/JS/Language_Fundamentals/FizzBuzz/script.js
@@ -0,0 +1,25 @@
+// Brute FORCE
+// for (var i=1; i<=20; i++){
+//     if (i % 3 != 0 && i % 5 != 0){
+//         console.log(i)
+//     }
+//     else {
+//         if (i % 3 != 0 && i % 5 == 0){
+//             console.log("Buzz")
+//         }
+//         else {
+//             if (i % 3 == 0 && i % 5 != 0){
+//                 console.log("Fizz")
+//             }
+//             else {console.log("FizzBuzz")}
+//         }
+//     }
+// }
+
+// Version 2
+for (var i=1; i<=20; i++){
+    // var out = ((i%3!=0) && i) || "Fizz" // Version2.1 (fail)
+    // var out = ( ((i%3==0) && "Fizz") + ((i%5==0) && "Buzz") ) || i // Version 2.2 (fail)
+    var out = (i%3!=0 && i%5!=0) && i || (i%3==0 && i%5==0) && "FizzBuzz" || (i%3!=0) && "Buzz" || "Fizz" // Version 2.3
+    console.log(out);
+}
