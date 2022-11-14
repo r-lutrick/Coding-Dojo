63b6823e58125ac7281ee148197753212a4ace42
Robert Lutrick <r.lutrick@icloud.com>
2022-11-14 14:03:13 -0600

    Updated Button Clicker

---
+++ Web_Fundamentals/JS/Document_Object_Model/Button_Clicker/script.js
@@ -0,0 +1,9 @@
+function loginOut (element) {
+    element.innerText = "Logout";
+}
+function addDefinition (element) {
+    element.remove();
+}
+function like (element) {
+    alert("Ninja was liked")
+}
