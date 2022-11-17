6d0890f87ef83a3fe12c673732564f42eb27dc0e
Robert Lutrick <r.lutrick@icloud.com>
2022-11-16 21:07:13 -0600

    Update

---
+++ Web_Fundamentals/JS/The_Yellow_Belt/script.js
@@ -0,0 +1,17 @@
+// Function for removing the Join Element
+function joinRemover(element) {
+    element.remove();
+}
+// A function used to incriment the provided ID's innerText
+function loveIt(id) {
+    document.querySelector(id).innerText++;
+}
+
+// Function for displaying the alert for the Search Bar
+function searchAlert() {
+    alert(
+        'You are searching for "' +
+            document.querySelector("#search-bar").value +
+            '"'
+    );
+}
