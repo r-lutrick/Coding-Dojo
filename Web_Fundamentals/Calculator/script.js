e07e119cdf80203571b81b90ddd2f259f8e3e7ce
Robert Lutrick <r.lutrick@icloud.com>
2022-11-27 10:15:06 -0600

    Added Calculator functions

--- Web_Fundamentals/Calculator/script.js
+++ Web_Fundamentals/Calculator/script.js
@@ -1,1 +1,49 @@
- 
+var reset = true;
+var a = 0;
+var op = '';
+
+function press(n) {
+    if (reset == true) {
+        document.querySelector('#display').innerText = n;
+        reset = false;
+    }
+    else {
+        document.querySelector('#display').innerText += n;
+        reset = false;
+    }
+}
+
+function clr(){
+    document.querySelector('#display').innerText = 0;
+    reset = true;
+}
+
+function setOP(opp) {
+    a = document.querySelector('#display').innerText;
+    op = opp;
+    reset = true;
+}
+
+function calculate() {
+    var b = document.querySelector('#display').innerText;
+    a = Number(a);
+    b = Number(b);
+    if (op == '+') {
+        var sum = a + b;
+        document.querySelector('#display').innerText = sum;
+        reset = true
+    } else if (op == '-') {
+        var sum = a - b;
+        document.querySelector('#display').innerText = sum;
+        reset = true;
+    } else if (op == '*') {
+        var sum = a * b;
+        document.querySelector('#display').innerText = sum;
+        reset = true;
+    } else if (op == '/') {
+        var sum = a / b;
+        document.querySelector('#display').innerText = sum;
+        reset = true;
+    }
+    
+}
