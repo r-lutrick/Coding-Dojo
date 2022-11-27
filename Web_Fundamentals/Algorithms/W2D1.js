c1b220e4adbc1c0e86b824cd464530805c4e20b3
Robert Lutrick <r.lutrick@icloud.com>
2022-11-27 09:53:38 -0600

    Update

--- Web_Fundamentals/Algorithms/W2D1.js
+++ Web_Fundamentals/Algorithms/W2D1.js
@@ -1,9 +1,9 @@
 function d6() {
     var roll = Math.random();
-    roll = Math.ceil(roll*6) //Ceiling is used here due to it not including 0. Floor could be used but a +1 would be needed because floor includes 0
+    roll = Math.ceil(roll * 6) //Ceiling is used here due to it not including 0. Floor could be used but a +1 would be needed because floor includes 0
     return roll;
 }
-    
+
 var playerRoll = d6();
 console.log("The player rolled: " + playerRoll);
 
@@ -30,13 +30,14 @@ var lifesAnswers = [
     "Very doubtful."
 ];
 
-function oracle (arr) {
+function oracle(arr) {
+    /*  
+    var i = Math.random()  < set variable i to be a random number (0.01 - 0.99)
+    i = i * arr.lenth      < update itself with lenth. Using arr.length allows for different sized arrays. 
+    i = Math.floor(i)      < update itself with floor. Floor is used here because we want to include the 0. 
+    Above can be written like this as well:
+    */
     var i = Math.floor(Math.random() * arr.length);
-    // Above can be written like this as well:
-    // var i = Math.random()  < set variable i to be a random number (0.01 - 0.99)
-    // i = i * arr.lenth      < update itself with lenth. Using arr.length allows for different sized arrays. 
-    // i = Math.floor(i)      < update itself with floor. Floor is used here because we want to include the 0.
-    
     return arr[i]
 }
 
