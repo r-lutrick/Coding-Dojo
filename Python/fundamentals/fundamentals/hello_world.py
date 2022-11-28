05607f95c7c0059b9fd4f738fb26ee175a056103
Robert Lutrick <r.lutrick@icloud.com>
2022-11-28 12:19:49 -0600

    Updates

---
+++ Python/fundamentals/fundamentals/hello_world.py
@@ -0,0 +1,10 @@
+print("Hello World")
+name = "Robert"
+print(f"Hello", name)
+print("Hello " + name +"!")
+num = 72
+print("Hello", num)
+print("Hello " + str(num))
+food_one = "Noodles"
+food_two = "Sushi"
+print(f"I love to eat {food_one} and {food_two}")
