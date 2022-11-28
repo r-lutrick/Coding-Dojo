0e813d2e7845573807ee242945b307e8b4d256ba
Robert Lutrick <r.lutrick@icloud.com>
2022-11-28 13:38:06 -0600

    Function Basics I Update

--- Python/fundamentals/fundamentals/functions_basic_i/functions_basic_i.py
+++ Python/fundamentals/fundamentals/functions_basic_i/functions_basic_i.py
@@ -2,46 +2,47 @@
 def number_of_food_groups():
     return 5
 print(number_of_food_groups())
+# 5
 
 
 #2
 def number_of_military_branches():
     return 5
 print(number_of_days_in_a_week_silicon_or_triangle_sides() + number_of_military_branches())
-
+# Undefined Error
 
 #3
 def number_of_books_on_hold():
     return 5
     return 10
 print(number_of_books_on_hold())
-
+# 5
 
 #4
 def number_of_fingers():
     return 5
     print(10)
 print(number_of_fingers())
-
+# 5
 
 #5
 def number_of_great_lakes():
     print(5)
 x = number_of_great_lakes()
 print(x)
-
+# 5
 
 #6
 def add(b,c):
     print(b+c)
 print(add(1,2) + add(2,3))
-
+# 8
 
 #7
 def concatenate(b,c):
     return str(b)+str(c)
 print(concatenate(2,5))
-
+# 25
 
 #8
 def number_of_oceans_or_fingers_or_continents():
@@ -53,7 +54,7 @@ def number_of_oceans_or_fingers_or_continents():
         return 10
     return 7
 print(number_of_oceans_or_fingers_or_continents())
-
+# 10
 
 #9
 def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
@@ -65,14 +66,16 @@ def number_of_days_in_a_week_silicon_or_triangle_sides(b,c):
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3))
 print(number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
 print(number_of_days_in_a_week_silicon_or_triangle_sides(2,3) + number_of_days_in_a_week_silicon_or_triangle_sides(5,3))
-
+# 7
+# 14
+# 21
 
 #10
 def addition(b,c):
     return b+c
     return 10
 print(addition(3,5))
-
+# 8
 
 #11
 b = 500
@@ -83,7 +86,10 @@ def foobar():
 print(b)
 foobar()
 print(b)
-
+# 500
+# 500
+# 300
+# 500
 
 #12
 b = 500
@@ -95,7 +101,10 @@ def foobar():
 print(b)
 foobar()
 print(b)
-
+# 500
+# 500
+# 300
+# 500
 
 #13
 b = 500
@@ -107,7 +116,10 @@ def foobar():
 print(b)
 b=foobar()
 print(b)
-
+# 500
+# 500
+# 300
+# 300
 
 #14
 def foo():
@@ -117,6 +129,9 @@ def foo():
 def bar():
     print(3)
 foo()
+# 1
+# 3
+# 2
 
 
 #15
@@ -130,3 +145,7 @@ def bar():
     return 5
 y = foo()
 print(y)
+# 1
+# 3
+# 5
+# 10
