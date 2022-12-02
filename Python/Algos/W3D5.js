5b693cb7129493b0416d08527960f634a9336b7f
Robert Lutrick <r.lutrick@icloud.com>
2022-12-02 10:15:58 -0600

    First Commit

---
+++ Python/Algos/W3D5.js
@@ -0,0 +1,42 @@
+/* 
+  String: Is Palindrome
+  Create a function that returns a boolean whether the string is a strict palindrome. 
+    - palindrome = string that is same forwards and backwards
+  
+  Do not ignore spaces, punctuation or capitalization
+*/
+// RIOT Read Input Output Talk
+const str1 = "a x a";
+const expected1 = true;
+
+const str2 = "racecar";
+const expected2 = true;
+
+const str3 = "Dud";
+const expected3 = false;
+
+const str4 = "oho!";
+const expected4 = false;
+
+const str5 = "abba"
+const expected5 = true;
+
+
+function isPalindrome(str) {
+    var revString = ""
+    for (let i = 0; i <= str.length - 1; i++) {
+        revString += str[str.length - 1 - i];
+    }
+    if (revString == str) {
+        return true
+    }
+    else{
+        return false
+    }
+}
+
+console.log(isPalindrome(str1)) //expected: true
+console.log(isPalindrome(str2)) //expected: true
+console.log(isPalindrome(str3)) //expected: false
+console.log(isPalindrome(str4)) //expected: false
+console.log(isPalindrome(str5)) //expected: true
