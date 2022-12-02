0368b7130b15a6116df8a2110fa29a1a5eefd3a7
Robert Lutrick <r.lutrick@icloud.com>
2022-12-01 20:04:47 -0600

    First Commit

---
+++ Python/Flask/fundamentals/html_table/static/script.js
@@ -0,0 +1,7 @@
+document.getElementById("bodystuff").addEventListener("mouseover", function() {
+    document.getElementById("bodystuff").style.backgroundColor = "grey";
+});
+    
+document.getElementById("bodystuff").addEventListener("mouseout", function() {
+    document.getElementById("bodystuff").style.backgroundColor = "white";
+});
