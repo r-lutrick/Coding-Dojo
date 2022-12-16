b5cb04d1e5988e74e698be258f3d2695464c5484
Robert Lutrick <r.lutrick@icloud.com>
2022-12-16 11:55:06 -0600

    Updated function to include all denominations

--- Python/Algos/Week5/W5D5.js
+++ Python/Algos/Week5/W5D5.js
@@ -25,53 +25,54 @@ const expected4 = { quarter: 3, dime: 2, penny: 4 };
  *    needed.
  */
 
-// function fewestCoinChange(cents) {
-//     coinDict = {
-//         'quarter': cents % 25 == 0 && cents / 25,
-//         'dime': cents % 10 == 0 && cents / 10,
-//         'nickle': cents % 5 == 0 && cents / 5,
-//         'penny': cents
-//     }
-
-//     return coinDict
-// }
-
-
 
 function fewestCoinChange(cents) {
-    coinDict = {}
-    while (cents > 0) {
-        if (cents >= 25) {
-            if (!('quarter' in coinDict)) {
-                coinDict['quarter'] = 1
-            }
-            coinDict['quarter'] += 1
-            cents -= 25
-        }
-        else if (cents >= 10) {
-            if (!('dime' in coinDict)) {
-                coinDict['dime'] = 1
-            }
-            coinDict['dime'] += 1
-            cents -= 10
-        }
-        else if (cents >= 5) {
-            if (!('nickle' in coinDict)) {
-                coinDict['nickle'] = 1
-            }
-            coinDict['nickle'] += 1
-            cents -= 5
-        }
-        else {
-            if (!('penny' in coinDict)) {
-                coinDict['penny'] = 1
-            }
-            coinDict['penny'] += 1
-            cents -= 1
-        }
+    coinDict = {
+        'quarter': cents >= 25 ? Math.floor(cents / 25) : 0,
+        'dime': cents >= 10 ? Math.floor(cents % 25 / 10) : 0,
+        'nickle': cents >= 5 ? Math.floor(cents % 25 % 10 / 5) : 0,
+        'penny': cents >= 1 && cents % 25 % 10 % 5 / 1
     }
+
     return coinDict
 }
+
+
+
+// function fewestCoinChange(cents) {
+//     coinDict = {}
+//     while (cents > 0) {
+//         if (cents >= 25) {
+//             if (!('quarter' in coinDict)) {
+//                 coinDict['quarter'] = 1
+//             }
+//             coinDict['quarter'] += 1
+//             cents -= 25
+//         }
+//         else if (cents >= 10) {
+//             if (!('dime' in coinDict)) {
+//                 coinDict['dime'] = 1
+//             }
+//             coinDict['dime'] += 1
+//             cents -= 10
+//         }
+//         else if (cents >= 5) {
+//             if (!('nickle' in coinDict)) {
+//                 coinDict['nickle'] = 1
+//             }
+//             coinDict['nickle'] += 1
+//             cents -= 5
+//         }
+//         else {
+//             if (!('penny' in coinDict)) {
+//                 coinDict['penny'] = 1
+//             }
+//             coinDict['penny'] += 1
+//             cents -= 1
+//         }
+//     }
+//     return coinDict
+// }
 
 console.log(fewestCoinChange(cents1)) // { quarter: 1 }
 console.log(fewestCoinChange(cents2)) // { quarter: 2 }
